a0736d5a8f289bc5a2caeba95960cb0e7d3a5d3f
vanacoca <catherine.vanacore@kellogg.northwestern.edu>
2020-02-23 22:42:46 +0000

    version 5

--- app.rb
+++ app.rb
@@ -12,30 +12,32 @@ ForecastIO.api_key = "3bb1b33e89ef956ed282a1a3c66e4827"
 get "/news" do
     #do everything else
 
+    #enter parameters and get latlong
     @results = Geocoder.search(params["q"])
     @location = params["q"]
     @lat_long = @results.first.coordinates # => [lat, long]
   
+    #get current forecast 
     @forecast = ForecastIO.forecast(@lat_long[0], @lat_long[1]).to_hash 
     @current_temperature = @forecast["currently"]["temperature"]
     @conditions = @forecast["currently"]["summary"]
-    "In #{@location}, it is currently #{@current_temperature} and #{@conditions}"
     
+    #get future forecast
     day_hightemp = []
     day_condition = []
     for day in @forecast["daily"]["data"]
         day_hightemp << "#{day["temperatureHigh"]}"
         day_condition << "#{day["summary"]}"
-    "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
+    #"A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
     end
     @listtemp = day_hightemp
     @listcondition = day_condition
-    #@day = Time.at(day["time"]).strftime("%a")
-    
+
+    #get the news
     @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=33043a374d84404e9df3c50b2cfac350"
     @news = HTTParty.get(@url).parsed_response.to_hash
-    pp @news
 
+    #create arrays for links and headlines
     headline_url = []
     for headlines in @news["articles"]
         headline_url << "#{headlines["url"]}"
@@ -47,7 +49,6 @@ get "/news" do
         headline_title << "#{headlinetitles["title"]}"
     end
     @listtitles = headline_title
-
 
     # news is now a Hash you can pretty print (pp) and parse for your output
  view "news"

c8c13eddcbe66be9c9c75f113de9581c4f5b5c89
vanacoca <catherine.vanacore@kellogg.northwestern.edu>
2020-02-23 18:41:22 +0000

    Version3

--- app.rb
+++ app.rb
@@ -14,14 +14,15 @@ get "/news" do
     # @location = params["q"]
     # @geocoder_results = Geocoder.search(@location)
     # @lat_long = @geocoder_results.first.coordinates # => [lat, long] array
-
+    view "news"
     @results = Geocoder.search(params["q"])
-    
+    @location = params["q"]
     @lat_long = @results.first.coordinates # => [lat, long]
-    @forecast = ForecastIO.forecast(@lat_long).to_hash #figure out how to put the lat long in here
-    @current_temperature = forecast["currently"]["temperature"]
-    @conditions = forecast["currently"]["summary"]
-    puts "In ___, it is currently #{@current_temperature} and #{@conditions}"
+  
+    @forecast = ForecastIO.forecast(@lat_long[0], @lat_long[1]).to_hash 
+    @current_temperature = @forecast["currently"]["temperature"]
+    @conditions = @forecast["currently"]["summary"]
+    puts "In #{@location}, it is currently #{@current_temperature} and #{@conditions}"
     #"#{@lat_long[0]} #{@lat_long[1]}"
     #url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=33043a374d84404e9df3c50b2cfac350"
     #news = HTTParty.get(url).parsed_response.to_hash
